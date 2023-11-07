1e20eeb580f42366e271635496d29a75abb42822
Artemonaaa <artemidaabc@gmail.com>
2023-11-07 13:29:36 +0200

    connected the database. Created table relationships

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,7 +11,27 @@ import { AuthModule } from './auth/auth.module';
 import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [UserModule, CategoryModule, AuthModule, TransactionModule],
+  imports: [
+    UserModule,
+    CategoryModule,
+    AuthModule,
+    TransactionModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        synchronize: true,
+        entities: [__dirname + '/**/*.entity{.js, .ts}']
+      }),
+      inject: [ConfigService]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
