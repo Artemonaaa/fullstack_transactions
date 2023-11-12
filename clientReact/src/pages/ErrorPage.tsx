cb1245a2b7ab15d3a6599eab8ed88e910aa3bdd0
Artemonaaa <artemidaabc@gmail.com>
2023-11-12 07:12:48 +0200

    Add router. Add component Header and page Error

--- clientReact/src/pages/ErrorPage.tsx
+++ clientReact/src/pages/ErrorPage.tsx
@@ -1,8 +1,18 @@
 import { FC } from 'react'
+import errorPageImg from '../assets/page_not_found.png'
+import { Link } from 'react-router-dom'
 
 const ErrorPage: FC = () => {
   return (
-    <div>ErrorPage</div>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-10 font-roboto text-white">
+      <img src={errorPageImg} alt="error" />
+      <Link
+        to={'/'}
+        className="rounded-md bg-slate-900 px-6 py-2 hover:bg-sky-600"
+      >
+        Back
+      </Link>
+    </div>
   )
 }
 
