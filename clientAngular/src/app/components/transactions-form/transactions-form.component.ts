75f843f1e8b83193a71b5e13d48fe110ce7b4ef1
Artemonaaa <artemidaabc@gmail.com>
2024-01-02 15:19:39 +0200

    Add the TailwindCSS

--- clientAngular/src/app/components/transactions-form/transactions-form.component.ts
+++ clientAngular/src/app/components/transactions-form/transactions-form.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-transactions-form',
   templateUrl: './transactions-form.component.html',
-  styleUrls: ['./transactions-form.component.scss']
 })
 export class TransactionsFormComponent {
 
