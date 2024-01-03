fbe0c259f855ce789aeaf129abb03582d938c422
Artemonaaa <artemidaabc@gmail.com>
2024-01-03 14:14:30 +0200

    Add the Header component

--- clientAngular/src/app/app.module.ts
+++ clientAngular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +29,8 @@ import { TransactionsTableComponent } from './components/transactions-table/tran
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- clientAngular/src/app/components/header/header.component.ts
+++ clientAngular/src/app/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { IconDefinition, faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-
+  isAuth: boolean = false
+  logoutIcon: IconDefinition = faArrowRightFromBracket
 }
